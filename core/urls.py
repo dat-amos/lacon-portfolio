5d17608dd26e25dc3077aedb348364156f900a26
dat-amos <amos.omofaiye@gmail.com>
2025-01-16 15:06:53 +0100

    Linked the web pages to home page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('services', services, name='services'),
+
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+# Home page
 def home(request):
     return render(request, 'index.html')
+
+# About us page
+def about(request):
+    return render(request, 'about.html')
+
+# Contact us page
+def contact(request):
+    return render(request, 'contact.html')
+
+# Our Services page
+def services(request):
+    return render(request, 'services.html')
 
